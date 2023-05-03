9e9b8ed6cd18e3e570a2a57dff8409ad8c3b496a
zjebnia <132460412+zjebnia@users.noreply.github.com>
2023-05-03 17:38:40 +0200

    website

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("copy-link").addEventListener("click", function() {
+    const textToCopy = "This is the text to be copied"; // Replace this with the text you want to copy
+    const newText = document.createElement("p");
+    newText.innerText = "ZKOPIOWANe!";
+    newText.style.color = "lime";
+    newText.style.textAlign = "center";
+    newText.id = "copied-text";
+    document.body.appendChild(newText);
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        console.log("Text copied to clipboard");
+        // You can also add an alert or other feedback message here
+      })
+      .catch((err) => {
+        console.error("Error copying text: ", err);
+      });
+  });
+  
+  document.getElementById("copy-link").addEventListener("mouseleave", function() {
+    const newText = document.getElementById("copied-text");
+    if (newText) {
+      newText.remove();
+    }
+  });
